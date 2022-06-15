0f4a4c6d37fba2322c3f5e5e1e2f506500bf6330
Himanshu Singh <hsnice16@gmail.com>
2022-06-15 19:19:45 +0000

    test: reducers

--- src/__tests__/reducers/authReducer.test.js
+++ src/__tests__/reducers/authReducer.test.js
@@ -17,4 +17,62 @@ describe("testing authReducer", () => {
 
     expect(state).toEqual(expectedState);
   });
+
+  it("should set status to success", () => {
+    const initialState = { status: "" };
+    const expectedState = { status: "success" };
+
+    const state = authReducer(initialState, {
+      type: ACTION_TYPE_SUCCESS,
+    });
+
+    expect(state).toEqual(expectedState);
+  });
+
+  it("should set status to error and error to given payload", () => {
+    const initialState = { status: "", error: null };
+    const expectedState = { status: "error", error: "testing error" };
+
+    const state = authReducer(initialState, {
+      type: ACTION_TYPE_ERROR,
+      payload: "testing error",
+    });
+
+    expect(state).toEqual(expectedState);
+  });
+
+  it("should enter form details", () => {
+    const initialState = {
+      status: "",
+      error: null,
+      firstName: "",
+      lastName: "",
+    };
+    const expectedState = {
+      status: "",
+      error: null,
+      firstName: "Inclusive",
+      lastName: "Mart",
+    };
+
+    const state = authReducer(initialState, {
+      type: ACTION_TYPE_ENTER_FORM_DETAILS,
+      payload: { firstName: "Inclusive", lastName: "Mart" },
+    });
+
+    expect(state).toEqual(expectedState);
+  });
+
+  it("should return the given state as it is", () => {
+    const initialState = {
+      status: "",
+      error: null,
+      firstName: "",
+      lastName: "",
+    };
+
+    const state = authReducer(initialState, { type: "" });
+
+    expect(state).toEqual(initialState);
+  });
 });
